06713a511397e0f32cdaf970f483ece9b904e16e
井田知希 <idachimare@idachimarenoMacBook-Air.local>
2017-08-01 20:50:15 +0900

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.0'
 set :application, 'achieve'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'hhttps://github.com/tomokiida/Achieve'
+set :repo_url, 'https://github.com/tomokiida/Achieve'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
