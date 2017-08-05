30ca08adede499a440a83977a14a60d6e68d13ea
井田知希 <idachimare@idachimarenoMacBook-Air.local>
2017-08-05 16:54:50 +0900

    dive20-2

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,7 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!
   def index
-    @users = User.all  
+    @users = User.all
     @conversations = Conversation.all
   end
 
@@ -13,7 +14,7 @@ class ConversationsController < ApplicationController
 
     redirect_to conversation_messages_path(@conversation)
   end
-  
+
   private
   def conversation_params
     params.permit(:sender_id, :recipient_id)
